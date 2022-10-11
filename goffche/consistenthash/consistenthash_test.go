96bd246816d4e36b1f00b8792d257e6fa6d609d5
L2ncE <llance_24@foxmail.com>
2022-10-11 17:49:02 +0800

    refactor: 删除注释

--- goffche/consistenthash/consistenthash_test.go
+++ goffche/consistenthash/consistenthash_test.go
@@ -11,7 +11,6 @@ func TestHashing(t *testing.T) {
 		return uint32(i)
 	})
 
-	// Given the above hash function, this will give replicas with "hashes":
 	// 2, 4, 6, 12, 14, 16, 22, 24, 26
 	hash.Add("6", "4", "2")
 
@@ -28,10 +27,8 @@ func TestHashing(t *testing.T) {
 		}
 	}
 
-	// Adds 8, 18, 28
 	hash.Add("8")
 
-	// 27 should now map to 8.
 	testCases["27"] = "8"
 
 	for k, v := range testCases {
